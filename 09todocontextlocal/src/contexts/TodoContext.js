945ff36ab505286b501b56a849951044aca401fe
vishnu <bvishnu123vardhan@gmail.com>
2024-07-06 23:16:01 +0530

    add : todos app

---
+++ 09todocontextlocal/src/contexts/TodoContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useContext } from "react";
+
+export const TodoContext=createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo Message",
+            completed:false
+        }
+    ],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+});
+
+//  default TodoContext;
+
+
+export const TodoProvider=TodoContext.Provider
+
+export const useTodo=()=>useContext(TodoContext)

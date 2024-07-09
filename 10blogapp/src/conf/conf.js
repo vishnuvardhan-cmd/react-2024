456b6659ab9563944dacc55c66171e38a56b2bd8
vishnu <bvishnu123vardhan@gmail.com>
2024-07-09 16:10:23 +0530

    add.functionality setup for login,logout for user

---
+++ 10blogapp/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteUrl: String(process.env.REACT_APP_APPWRTIE_URL),
+  appwriteDatabaseId: String(process.env.REACT_APP_APPWRTIE_DATABASE_ID),
+  appwriteProjectId: String(process.env.REACT_APP_APPWRTIE_PROJECT_ID),
+  appwriteCollectionId: String(process.env.REACT_APP_APPWRTIE_COLLECTION_ID),
+  appwriteBucketId: String(process.env.REACT_APP_APPWRTIE_BUCKET_ID),
+};
+
+export default conf;
